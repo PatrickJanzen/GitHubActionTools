8506d9382dd7113f5cd69a739fdf73a1e6319336
Patrick Janzen <patrick.janzen@rewe-digital.com>
2024-03-19 12:51:54 +0100

    initial commit

---
+++ src/Logger/Logger.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GitHubWorkflowTools\Logger;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Logger
+{
+
+    const GROUP = 'group';
+    const NOTICE = 'notice';
+    const WARNING = 'warning';
+    const ERROR = 'error';
+    const DEBUG = 'debug';
+    const END_GROUP = 'endgroup';
+    private static int $groupCounter = 0;
+
+    public function __construct(private OutputInterface $out)
+    {
+
+    }
+
+    private static function addIfSet(array &$data, string $template, null|string|int $value)
+    {
+        if ($value !== null) {
+            $data[] = sprintf($template, $value);
+        }
+    }
+
+    public function debug(string $message): void
+    {
+        $this->log(self::DEBUG, $message);
+    }
+
+    public function log(string $type, string $message = '', ?string $filename = null, ?int $col = null, ?int $endCol = null, ?int $line = null, ?int $endLine = null, ?string $title = null): void
+    {
+        $data = [];
+
+        self::addIfSet($data, 'file=%s', $filename);
+        self::addIfSet($data, 'line=%s', $line);
+        self::addIfSet($data, 'endLine=%s', $endLine);
+        self::addIfSet($data, 'title=%s', $title);
+        self::addIfSet($data, 'col=%s', $col);
+        self::addIfSet($data, 'endCol=%s', $endCol);
+
+        $payload = implode(',', $data);
+        if (!empty($payload)) {
+            $payload = ' ' . $payload;
+        }
+        $this->write(sprintf('::%s%s::%s', $type, $payload, $message));
+    }
+
+    public function write(string $output): void
+    {
+        $this->out->writeln($output);
+    }
+
+    public function notice(string $message, ?string $filename = null, ?int $col = null, ?int $endCol = null, ?int $line = null, ?int $endLine = null, ?string $title = null): void
+    {
+        $this->log(self::NOTICE, $message, $filename, $col, $endCol, $line, $endLine, $title);
+    }
+
+    public function warning(string $message, ?string $filename = null, ?int $col = null, ?int $endCol = null, ?int $line = null, ?int $endLine = null, ?string $title = null): void
+    {
+        $this->log(self::WARNING, $message, $filename, $col, $endCol, $line, $endLine, $title);
+    }
+
+    public function error(string $message, ?string $filename = null, ?int $col = null, ?int $endCol = null, ?int $line = null, ?int $endLine = null, ?string $title = null): void
+    {
+        $this->log(self::ERROR, $message, $filename, $col, $endCol, $line, $endLine, $title);
+    }
+
+    public function startGroup(?string $groupName = null): void
+    {
+        $this->log(self::GROUP, $groupName ?? sprintf('group %d', ++self::$groupCounter));
+    }
+
+    public function endGroup(): void
+    {
+        $this->log(self::END_GROUP);
+    }
+}
---
+++ test/Logger/LoggerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GitHubWorkflowTools\Test\Logger;
+
+use GitHubWorkflowTools\Logger\Logger;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @covers \GitHubWorkflowTools\Logger\Logger
+ */
+class LoggerTest extends MockeryTestCase
+{
+
+    /**
+     * @test
+     * @dataProvider data
+     */
+    public function loggerTest(string $method, array $logData, string $expectation)
+    {
+        $output = mock(OutputInterface::class);
+
+        $output->shouldReceive('writeln')->once()->with($expectation);
+
+        $subject = new Logger($output);
+        $this->assertTrue(method_exists($subject, $method));
+        $this->assertTrue(is_callable([$subject, $method]));
+        $subject->$method(...$logData);
+    }
+
+    public function data(): \Generator
+    {
+        yield ['startGroup', ['group1'], '::group::group1'];
+        yield ['write', ['test'], 'test'];
+        yield ['warning', ['warning test', 'somefile.src', 0, 10, 0, 1, 'title'], '::warning file=somefile.src,line=0,endLine=1,title=title,col=0,endCol=10::warning test'];
+        yield ['notice', ['notice test'], '::notice::notice test'];
+        yield ['error', ['error test'], '::error::error test'];
+        yield ['debug', ['debug test'], '::debug::debug test'];
+        yield ['log', ['unknown', 'unknown test'], '::unknown::unknown test'];
+        yield ['endGroup', [], '::endgroup::'];
+        yield ['startGroup', [ null ], '::group::group 1'];
+    }
+}
